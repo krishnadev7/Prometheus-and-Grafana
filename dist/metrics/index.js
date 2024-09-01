57afca0d17c6f926f12ab42bd7fd256170175f4e
krishnadev7 <krishnadevv12@gmail.com>
2024-09-01 21:59:52 +0530

    added histogram and dockerfile

--- dist/metrics/index.js
+++ dist/metrics/index.js
@@ -1,8 +1,31 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.cleanupMiddleware = void 0;
+exports.cleanupMiddleware = exports.metricsMiddleware = void 0;
 const requestCount_1 = require("./requestCount");
 const activeRequests_1 = require("./activeRequests");
+const requestCount_2 = require("./requestCount");
+const metricsMiddleware = (req, res, next) => {
+    const startTime = Date.now();
+    activeRequests_1.activeRequestsGauge.inc();
+    res.on('finish', function () {
+        const endTime = Date.now();
+        const duration = endTime - startTime;
+        // Increment request counter
+        requestCount_1.requestCounter.inc({
+            method: req.method,
+            route: req.route ? req.route.path : req.path,
+            status_code: res.statusCode
+        });
+        requestCount_2.httpRequestDurationMicroseconds.observe({
+            method: req.method,
+            route: req.route ? req.route.path : req.path,
+            code: res.statusCode
+        }, duration);
+        activeRequests_1.activeRequestsGauge.dec();
+    });
+    next();
+};
+exports.metricsMiddleware = metricsMiddleware;
 const cleanupMiddleware = (req, res, next) => {
     const startTime = Date.now();
     activeRequests_1.activeRequestsGauge.inc();
